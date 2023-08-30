77326393ebb4490dfb5c4c746a853487c3b5d907
daro-decagon <gadibiao@decagonhq.com>
2023-08-30 06:07:16 +0100

    fixes authorization issues

--- src/main/java/com/deca/securitysquad16/config/OpenAPIConfig.java
+++ src/main/java/com/deca/securitysquad16/config/OpenAPIConfig.java
@@ -1,8 +1,6 @@
 package com.deca.securitysquad16.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
