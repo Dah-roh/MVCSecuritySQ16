ab2abd2912bc51499c79da7302c74d30c874804e
Dah-roh <darogadibia@gmail.comi>
2023-09-03 14:03:44 +0100

    fixes rest security: adds fixes swagger securityScheme and adds SecurityRequirement annotation to controller, fixes mvc security: removes space from "ROLE_USER " and adds link to signup page

--- src/main/java/com/deca/securitysquad16/SecuritySquad16Application.java
+++ src/main/java/com/deca/securitysquad16/SecuritySquad16Application.java
@@ -2,8 +2,10 @@ package com.deca.securitysquad16;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class SecuritySquad16Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/deca/securitysquad16/aop/LogAspect.java
@@ -0,0 +1,34 @@
+package com.deca.securitysquad16.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+    @Before("execution(* com.deca.securitysquad16.controllers.HomeController.addNewUser(..)) && args(com.deca.securitysquad16.DTOs.UsersDTO)")
+    public void beforeAddingNewUser(JoinPoint point){
+       log.info("This is about add a new user..."+ Arrays.toString(point.getArgs()) );
+    }
+
+
+    @After(value = "execution(* com.deca.securitysquad16.controllers.HomeController.addNewUser(..)) && args(com.deca.securitysquad16.DTOs.UsersDTO))")
+    public void afterAddingNewUser(JoinPoint joinPoint){
+        log.info(("Finished adding a new user...%s".formatted(joinPoint.getArgs())));
+        System.out.println();
+    }
+
+    @AfterThrowing(value = "execution(* com.deca.securitysquad16.controllers.HomeController.addNewUser(..))", throwing = "ex")
+    public void addingNewUserException(Exception ex){
+        log.info(("Method threw exception...%s, %s".formatted( ex.getMessage())));
+
+    }
+}
